c0b15d7c0c4ec17b328568ca3cd2da13e6ac6ce5
Matteo <ricciottimatteo2003@gmail.com>
2023-01-19 15:28:27 +0100

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./app/components/**/*.{html,js}",
-      "./app/pages/**/*.{html,js}",
-      "./app/index.html",
-    ],
-    theme: {
-      colors: {
-        "primary-base": "#9333ea",
-        "primary-hover": "#a855f7",
-        "primary-disabled": "#d8b4fe",
-        "primary-background": "#f3e8ff",
-        "dark-grey-base": "#1f2937",
-        "dark-grey-placeholder": "#4b5563",
-        "dark-grey-disabled": "#9ca3af",
-        "error": "#e11d48",
-        "success": "#22c55e",
-        "warning": "#f59e0b"
-      },
-      extend: {},
-    },
-    plugins: [],
-  };
+	content: ['./app/components/**/*.{html,js}', './app/pages/**/*.{html,js}', './app/index.html'],
+	theme: {
+		colors: {
+			'primary-base': '#9333ea',
+			'primary-hover': '#a855f7',
+			'primary-disabled': '#d8b4fe',
+			'primary-background': '#f3e8ff',
+			'dark-grey-base': '#1f2937',
+			'dark-grey-placeholder': '#4b5563',
+			'dark-grey-disabled': '#9ca3af',
+			error: '#e11d48',
+			success: '#22c55e',
+			warning: '#f59e0b'
+		},
+		extend: {}
+	},
+	plugins: []
+};
