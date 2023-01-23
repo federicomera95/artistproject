815af97950491dde952932dddd4d59628e882c90
Matteo <ricciottimatteo2003@gmail.com>
2023-01-23 16:52:02 +0100

    setup react

---
+++ README.md
@@ -0,0 +1,11 @@
+# By: Matteo
+
+-   Ho fatto un setup base del progetto e ripulito alcuni file pre-scritti da react,
+    inoltre ho messo un'icona molto a caso presa da google in attesa della scelta del logo
+-   Per lanciare il progetto scrivete nel terminale
+
+```
+yarn dev
+```
+
+-   Se avete qualcosa da dire agli altri scrivetela qua cancellando il testo corrente
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                custom: 'Rubik'
+            },
+            colors: {
+                'primary-base': '#9333ea',
+                'primary-hover': '#a855f7',
+                'primary-disabled': '#d8b4fe',
+                'primary-background': '#f3e8ff',
+                'dark-grey-base': '#1f2937',
+                'dark-grey-placeholder': '#4b5563',
+                'dark-grey-disabled': '#9ca3af',
+                error: '#e11d48',
+                success: '#22c55e',
+                warning: '#f59e0b',
+                white: '#ffffff'
+            },
+            padding: {
+                ms: '10px 16px',
+                lg: '10px 24px'
+            },
+            boxShadow: {
+                hover: '0 4px 8px',
+                focus: '0 0 0 4px'
+            }
+        }
+    }
+};
