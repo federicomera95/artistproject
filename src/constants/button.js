fdee68acc9340bc77dae0c4236c56db54e8da935
Federico Merafina <federico.merafina@losectrl.it>
2023-01-23 17:13:03 +0100

    feat: create consts for ButtonComponent

---
+++ src/constants/button.js
@@ -0,0 +1,11 @@
+export const SIZE = {
+    small: 'btn-sm',
+    medium: 'btn-md',
+    large: 'btn-lg',
+};
+
+export const STYLE = {
+    primary: 'btn-primary',
+    secondary: 'btn-secondary',
+    tertiary: 'btn-tertiary',
+};
