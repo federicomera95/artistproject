a5ccba0521e447a054618b3a7176e55f30c7bfc5
Federico Merafina <federico.merafina@losectrl.it>
2023-01-24 17:09:31 +0100

    fix: update disabled params

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <ButtonComponent text='Button' size='large' style='primary' isDisabled={false} />
-        <ButtonComponent text='Button' size='large' style='primary' isDisabled={true} />
+        <ButtonComponent text='Button' size='large' style='primary' />
+        <ButtonComponent text='Button' size='large' style='primary' disabled />
     </React.StrictMode>
 );
