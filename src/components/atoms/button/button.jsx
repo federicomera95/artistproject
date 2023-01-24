2773c927c94e2e88f7ffa5b44c5a75bd82c577aa
Federico Merafina <federico.merafina@losectrl.it>
2023-01-24 17:09:53 +0100

    fix: rename isDisabled in disabled

--- src/components/atoms/button/button.jsx
+++ src/components/atoms/button/button.jsx
@@ -1,9 +1,9 @@
 import './button.css';
 import { SIZE, STYLE } from '../../../constants/button';
 
-export function ButtonComponent({ id, type='button', text, size, style, isDisabled }) {
+export function ButtonComponent({ id, type='button', text, size, style, disabled }) {
     return (
-        <button id={id} className={`${SIZE[size]} ${STYLE[style]}`} disabled={isDisabled} type={type}>
+        <button id={id} className={`${SIZE[size]} ${STYLE[style]}`} disabled={disabled} type={type}>
             {text}
         </button>
     );
