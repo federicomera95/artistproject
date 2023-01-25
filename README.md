d904fe8539a3957410d515acc7c04d6fd7c672e9
Matteo <ricciottimatteo2003@gmail.com>
2023-01-25 09:28:45 +0100

    reset di index.jsx

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# By: Matteo
+# Info
 
--   Ho fatto un setup base del progetto e ripulito alcuni file pre-scritti da react,
-    inoltre ho messo un'icona molto a caso presa da google in attesa della scelta del logo
--   Per lanciare il progetto scrivete nel terminale
+-   Per lanciare il progetto scrivete nel terminale:
 
 ```
 yarn dev
 ```
-
--   Se avete qualcosa da dire agli altri scrivetela qua cancellando il testo corrente
--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,8 @@ import React from 'react';
 
 import './global.css';
 
-import { ButtonComponent } from './components/atoms/button/button';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>
-        <ButtonComponent text='Button' size='large' style='primary' />
-        <ButtonComponent text='Button' size='large' style='primary' disabled />
-    </React.StrictMode>
+    <React.StrictMode>Ricordatevi di cancellare quello che avete fatto qua dentro</React.StrictMode>
 );
