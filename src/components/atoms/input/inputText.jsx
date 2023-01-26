3ad2122f7e565c87601242f1e28e0bece5415fab
Federico Merafina <federico.merafina@losectrl.it>
2023-01-26 11:04:37 +0100

    fix: update condition pos === 'rt' to 'right' and 'lf' to 'left'

--- src/components/atoms/input/inputText.jsx
+++ src/components/atoms/input/inputText.jsx
@@ -16,9 +16,9 @@ export default function InputTextComponent({ objInputText, disabled, required, i
         <div className='input-field-group'>
             <label htmlFor={id} disabled={disabled}>{label}</label>
             <div className='input-text-group'>
-                {(!!icon && pos === 'lf') && <i className={`${src} ${pos}`}></i>}
+                {(!!icon && pos === 'left') && <i className={`${src} ${pos}`}></i>}
                 <input className={ `${msgError ? 'error' : ''} ${value && 'filled'}`} type={type} id={id} disabled={disabled} required={required} onChange={handleChange}/>
-                {(!!icon && pos === 'rt') && <i className={`${src} ${pos}`}></i>}
+                {(!!icon && pos === 'right') && <i className={`${src} ${pos}`}></i>}
                 <label className={ `${value && 'filled'} ${!!icon && pos}`} disabled={disabled}>{`Inserire ${label}`}</label>
             </div>
             { !!msgError && <p>{msgError}</p> }
