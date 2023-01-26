e61ee4ced80e4f07a030c5f01df6fc8caa8b2437
Federico Merafina <federico.merafina@losectrl.it>
2023-01-26 17:17:50 +0100

    feat: add default value of input, match with useState value default for onchange condition to fill and insert title attribute passed from parent component

--- src/components/atoms/input/inputText.jsx
+++ src/components/atoms/input/inputText.jsx
@@ -5,8 +5,8 @@ import { useState } from 'react';
 export default function InputTextComponent({ objInputText, disabled, required, icon = {} }) {
 
     const {pos, src} = icon;
-    const {id, type='text', label, msgError=''} = objInputText;
-    const [value, setValue] = useState('');
+    const {id, type='text', label, placeholder, msgError='', val} = objInputText;
+    const [value, setValue] = useState(val);
 
     const handleChange = (e) => setValue(e.target.value);
 
@@ -17,9 +17,16 @@ export default function InputTextComponent({ objInputText, disabled, required, i
             <label htmlFor={id} disabled={disabled}>{label}</label>
             <div className='input-text-group'>
                 {(!!icon && pos === 'left') && <i className={`${src} ${pos}`}></i>}
-                <input className={ `${msgError ? 'error' : ''} ${value && 'filled'}`} type={type} id={id} disabled={disabled} required={required} onChange={handleChange}/>
+                <input 
+                    title={placeholder} 
+                    className={ `${msgError ? 'error' : ''} ${value && 'filled'}`} 
+                    type={type} id={id}
+                    defaultValue={value} 
+                    disabled={disabled} 
+                    required={required} 
+                    onChange={handleChange}/>
                 {(!!icon && pos === 'right') && <i className={`${src} ${pos}`}></i>}
-                <label className={ `${value && 'filled'} ${!!icon && pos}`} disabled={disabled}>{`Inserire ${label}`}</label>
+                <label className={ `${value && 'filled'} ${!!icon && pos}`} disabled={disabled}>{placeholder}</label>
             </div>
             { !!msgError && <p>{msgError}</p> }
         </div>
