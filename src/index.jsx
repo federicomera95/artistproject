b97b987d76b963fd2eeb568ca3db61e0f145cd22
Federico Merafina <federico.merafina@losectrl.it>
2023-01-30 15:40:00 +0100

    feat: remove react strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import React from 'react';
 
 import './global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>Ricordatevi di cancellare quello che avete fatto qua dentro</React.StrictMode>
+    <p>Ricordatevi di cancellare quello che avete fatto qua dentro</p>
 );
