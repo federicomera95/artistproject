9dcb3df9dbaeadf7cfaf6dc8bc43ce7c859d8ca1
Matteo <ricciottimatteo2003@gmail.com>
2023-02-07 14:38:10 +0100

    fix range slider

--- src/components/atoms/rangeSlider/rangeSlider.jsx
+++ src/components/atoms/rangeSlider/rangeSlider.jsx
@@ -5,9 +5,9 @@ import { useState } from 'react';
 const RangeSliderComponent = ({
     label,
     id,
-    defaultValues = [15, 70],
-    minValue = 15,
-    maxValue = 70,
+    defaultValues = [26, 48],
+    minValue = 18,
+    maxValue = 65,
     minDistance = 1,
     disabled
 }) => {
@@ -20,42 +20,105 @@ const RangeSliderComponent = ({
             `Prop 'defaultValues' must contain 2 numbers between 'minValue' and 'maxValue'`
         );
 
-    const [values, setValues] = useState({ min: defaultValues[0], max: defaultValues[1] });
+    const [min, setMin] = useState(defaultValues[0]);
+    const [max, setMax] = useState(defaultValues[1]);
 
     const handleChange = ({ target }) => {
-        if (values.max - values.min <= minDistance ) {
-            if(target.id === 'min'){
-                if(target.value < values.max && target.value < values.min){
-                    setValues({...values,[target.id]: target.value});
-                }else{
-                    setValues({...values,[target.id]: values.max - minDistance});
+        if (max - min <= minDistance) {
+            if (target.id === 'min') {
+                if (target.value < max && target.value < min) {
+                    setMin(target.value);
+                } else {
+                    setMin(max - minDistance);
                 }
             }
-            if(target.id === 'max'){
-                if(target.value > values.max && target.value > values.min){
-                    setValues({...values,[target.id]: target.value});
-                }else{
-                    setValues({...values,[target.id]: values.min + minDistance});
+            if (target.id === 'max') {
+                if (target.value > max && target.value > min) {
+                    setMax(target.value);
+                } else {
+                    setMax(min + minDistance);
                 }
             }
-            //&& (target.value < values.max && target.value > values.min)
-            // setValues({...values,
-            //     [target.id]:target.id === 'min' ? Number(values.max - minDistance) : Number(values.min + minDistance)
-            // });
         } else {
-            setValues({...values,[target.id]: Number(target.value)
-            });
+            target.id === 'min' ? setMin(Number(target.value)) : setMax(Number(target.value));
         }
+    };
+
+    const getLeft = () => {
+        const base = ((min - minValue) * 100) / maxValue;
+        const expr = ((minValue / 10) * 100) / maxValue;
+        let fixed = base;
+
+        if (base <= 10) {
+            fixed += expr * 1;
+        } else if (base > 10 && base <= 20) {
+            fixed += expr * 2.2;
+        } else if (base > 20 && base <= 30) {
+            fixed += expr * 3.4;
+        } else if (base > 30 && base <= 40) {
+            fixed += expr * 4.6;
+        } else if (base > 40 && base <= 50) {
+            fixed += expr * 5.8;
+        } else if (base > 50 && base <= 60) {
+            fixed += expr * 7;
+        } else if (base > 60 && base <= 70) {
+            fixed += expr * 8.2;
+        } else if (base > 70 && base <= 80) {
+            fixed += expr * 9.4;
+        } else if (base > 80 && base <= 90) {
+            fixed += expr * 10.6;
+        } else if (base > 90 && base <= 100) {
+            fixed += expr * 11.8;
+        }
+        return fixed;
+    };
+
+    const getRight = () => {
+        const base = 100 - (max * 100) / maxValue;
+        const expr = (100 * maxValue) / (100 * minValue);
+        let fixed = base;
+
+        if (base <= 10) {
+            fixed += expr * 1;
+        } else if (base > 10 && base <= 20) {
+            fixed += expr * 2;
+        } else if (base > 20 && base <= 30) {
+            fixed += expr * 3;
+        } else if (base > 30 && base <= 40) {
+            fixed += expr * 4;
+        } else if (base > 40 && base <= 50) {
+            fixed += expr * 5;
+        } else if (base > 50 && base <= 60) {
+            fixed += expr * 6;
+        } else if (base > 60 && base <= 70) {
+            fixed += expr * 7;
+        } else if (base > 70 && base <= 80) {
+            fixed += expr * 8;
+        } else if (base > 80 && base <= 90) {
+            fixed += expr * 9;
+        } else if (base > 90 && base <= 100) {
+            fixed += expr * 10;
+        }
+
+        return fixed;
     };
 
     return (
         <div className='slider-wrapper'>
+            <div className='flex relative font-semibold text-sm text-primary-base'>
+                <p className='absolute bottom-2' style={{ left: `${getLeft()}%` }}>
+                    {min}
+                </p>
+                <p className='absolute bottom-2' style={{ right: `${getRight()}%` }}>
+                    {max}
+                </p>
+            </div>
             <div className='slider'>
                 <div
                     className='track'
                     style={{
-                        left: `${((values.min - minValue) / 100) * 100}%`, // 0 -> 100
-                        right: `${100 - (values.max / maxValue) * 100}%` // 
+                        left: `${getLeft()}%`,
+                        right: `${getRight()}%`
                     }}
                 ></div>
             </div>
@@ -66,7 +129,7 @@ const RangeSliderComponent = ({
                     className='range-min'
                     min={minValue}
                     max={maxValue}
-                    value={values.min}
+                    value={min}
                     onChange={handleChange}
                 />
                 <input
@@ -75,7 +138,7 @@ const RangeSliderComponent = ({
                     className='range-max'
                     min={minValue}
                     max={maxValue}
-                    value={values.max}
+                    value={max}
                     onChange={handleChange}
                 />
             </div>
