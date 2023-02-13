043abcae38dddeb1c6a0560d31b5caf0a7a8cea9
Matteo <ricciottimatteo2003@gmail.com>
2023-02-13 16:45:00 +0100

    atom: city select

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
 import './global.css';
-import AddContentAudio from './pages/add-content-audio';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<>
-        <AddContentAudio/>
-</>);
+root.render(<></>);
