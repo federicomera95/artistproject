e4a4a99c7c12db2266c56d6d4e2280abd1557301
Matteo <ricciottimatteo2003@gmail.com>
2023-02-20 17:34:57 +0100

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import { TextAreaComponent } from './components/atoms/textarea/textarea';
-
 
 import './global.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<>
-<div className='p-[24px]'>
-    <TextAreaComponent />
-</div>
-</>);
+root.render(
+    <>
+        <div className='p-5'></div>
+    </>
+);
