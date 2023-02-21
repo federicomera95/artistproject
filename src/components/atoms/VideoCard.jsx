59a1599594f50c5b0a777cf34f0378061e21231c
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-02-21 22:35:21 +0100

    Add VideoCard Component

--- src/components/atoms/VideoCard.jsx
+++ src/components/atoms/VideoCard.jsx
@@ -1,5 +1,5 @@
-export const PhotoProfile = ({artName, photoTitle}) => {
-    return (
+const VideoCard = ({artName, videoTitle}) => {
+    return(
         <div className='flex gap-4 overflow-x-scroll'>
             <div className='flex - flex-col gap-1'>
                 <div className='flex flex-row items-center gap-1'>
@@ -7,11 +7,13 @@ export const PhotoProfile = ({artName, photoTitle}) => {
                     <p className='text-[10px]'>{artName}</p>
                 </div>
                 <div className='flex gap-4'>
-                    <div className='w-[150px] shrink-0 h-[150px] bg-slate-400 gap-1 rounded-lg '></div>
+                    <div className='w-[150px] shrink-0 h-[84px] bg-slate-400 gap-1 rounded-lg '></div>
                 </div>
-                <p className='text-[12px] mt-[4px]'>{photoTitle}</p>
+                <p className='text-[12px] mt-[4px]'>{videoTitle}</p>
             </div>
         </div>
     );
 };
+
+export default VideoCard;/* Frame 65 */
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import { PhotoProfile } from './components/atoms/PhotoCard';
 import './global.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <>
         <div className='p-5'>
             {/* <PhotoProfile artName="Giuseppe" photoTitle="Photocard Component"/> */}
+            {/* <VideoCard artName="Giuseppe" videoTitle="VideoCard-Component" /> */}
         </div>
     </>
 );
