75b5d928687c2297827d5630f9a8c8fccc3a8c47
Giuseppe Russo <116862150+iTzRusso10@users.noreply.github.com>
2023-03-07 19:23:05 +0100

    prettier

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,16 +1,15 @@
-import Button from "../components/atoms/Button";
+import Button from '../components/atoms/Button';
 
-
-const Success = ({text}) => {
-    return(
-        <div className="flex flex-col gap-[48px] justify-center items-center h-screen">
-            <div className="flex flex-col justify-center items-center">
-                <img src="/success.svg" className="w-[100px] h-[100px]"></img>
-                <p className="text-2xl font-semibold">{text}</p>
+const Success = ({ text }) => {
+    return (
+        <div className='flex flex-col gap-[48px] justify-center items-center h-screen'>
+            <div className='flex flex-col justify-center items-center'>
+                <img src='/success.svg' className='w-[100px] h-[100px]'></img>
+                <p className='text-2xl font-semibold'>{text}</p>
             </div>
-            <div className="flex flex-col gap-4">
-                <Button text="Carica un'altra foto" style="primary" size="medium"></Button>
-                <Button text="Torna al profilo" style="secondary"></Button>
+            <div className='flex flex-col gap-4'>
+                <Button text="Carica un'altra foto" style='primary' size='medium'></Button>
+                <Button text='Torna al profilo' style='secondary'></Button>
             </div>
         </div>
     );
