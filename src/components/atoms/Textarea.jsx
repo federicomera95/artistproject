291c953b01a46743cf67684aeeeda5fc1e577f56
Federico Merafina <federico.merafina@losectrl.it>
2023-03-09 12:06:43 +0100

    fix: textarea with max-h, size text and line

--- src/components/atoms/Textarea.jsx
+++ src/components/atoms/Textarea.jsx
@@ -11,18 +11,20 @@ const Textarea = ({ id, label, error, placeholder, disabled, type }) => {
     }, [value]);
 
     return (
-        <div className='flex flex-col-reverse gap-2'>
+        <div className='flex flex-col-reverse gap-2 text-[16px] leading-[19px]'>
             <div className='flex flex-col gap-2'>
                 <textarea
                     id={id}
                     onChange={(e) => setValue(e.target.value)}
                     placeholder={placeholder}
                     value={value}
+                    rows={30}
+                    cols={30}
                     type={type}
                     disabled={disabled}
                     className={`peer ${value.length > 0 ? 'border-dark-grey-placeholder' : ''} ${
                         err && !disabled ? 'border-error' : 'border-dark-grey-disabled'
-                    } py-[10px] px-4 border-2 rounded-lg 
+                    } py-[10px] px-4 border-2 rounded-lg max-h-[135px]
                     placeholder:text-dark-grey-placeholder
                     hover:border-primary-hover focus:outline-none
                     focus:border-primary-base focus:shadow-hover focus:shadow-primary-base/30
