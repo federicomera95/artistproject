97b63e36620dfd49d3bbe7183f68a46b1500e326
Federico Merafina <federico.merafina@losectrl.it>
2023-03-10 10:10:10 +0100

    fix: change input file component props (ref,val,error,change and setState for name )

--- src/components/atoms/InputFile.jsx
+++ src/components/atoms/InputFile.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from 'react';
 import { Upload } from '../../assets/icons';
 
-const InputFile = ({ id, label, error = '', required, text }) => {
+const InputFile = ({ inputFileProps }) => {
+    const { id, label, val, acceptRules, error = '', change, forwardedRef, text } = inputFileProps;
+
     if (!id) throw new Error('The id is required!');
 
-    const [fileValue, setFileValue] = useState();
     const [preview, setPreview] = useState();
+    const [fileName, setName] = useState();
 
     useEffect(() => {
-        if (!fileValue) return setPreview(undefined);
-
-        const objectUrl = URL.createObjectURL(fileValue);
+        if (!val || error) {
+            setPreview(undefined);
+            setName('');
+            return;
+        }
+        if (id === 'audio' || id === 'video') return setName(val?.name);
+        const objectUrl = URL.createObjectURL(val);
         setPreview(objectUrl);
-
         return () => URL.revokeObjectURL(objectUrl);
-    }, [fileValue]);
-
-    const handleChange = ({ files }) => setFileValue(files[0]);
+    }, [val, error, id]);
 
     return (
         <div className='w-[100%] flex flex-col gap-[8px] text-[14px]'>
@@ -27,7 +30,9 @@ const InputFile = ({ id, label, error = '', required, text }) => {
                 {label}
             </label>
             <div
-                className='flex items-center justify-center border-[2px] border-dashed border-dark-grey-disabled bg-no-repeat bg-center'
+                className={`flex items-center justify-center border-[2px] border-dashed border-dark-grey-disabled bg-no-repeat bg-center ${
+                    error && 'border-error'
+                }`}
                 style={{
                     backgroundImage: `url(${preview ? preview : '/assets/cover-default.svg'})`
                 }}
@@ -35,17 +40,20 @@ const InputFile = ({ id, label, error = '', required, text }) => {
                 <input
                     className='pointer-events h-[139px] opacity-0 z-[1]'
                     id={id}
+                    name={id}
+                    accept={acceptRules}
                     type='file'
-                    onChange={({ target }) => handleChange(target)}
-                    required={required}
+                    onChange={change}
+                    ref={forwardedRef}
                 />
                 {!preview && (
-                    <div className='absolute'>
+                    <div className='absolute flex flex-col justify-center items-center'>
                         <Upload />
+                        {fileName}
                     </div>
                 )}
             </div>
-            <p className='text-dark-grey-placeholder'>{text}</p>
+            <p className={`${error ? 'text-error' : 'text-dark-grey-placeholder'}`}>{text}</p>
         </div>
     );
 };
