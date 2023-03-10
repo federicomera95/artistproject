888dfb01aa4a611c079f162b12ee1d048a5157d6
Federico Merafina <federico.merafina@losectrl.it>
2023-03-10 10:11:32 +0100

    fix: textarea props (add the same params of inputText component)

--- src/components/atoms/Textarea.jsx
+++ src/components/atoms/Textarea.jsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-const Textarea = ({ id, label, error, placeholder, disabled, type }) => {
-    const [err, setError] = useState(false);
-    const [value, setValue] = useState('');
+const Textarea = ({ textareaProps }) => {
+    const { id, label, val, error, placeholder, change, forwardedRef, blur, disabled } =
+        textareaProps;
 
     if (!id) throw new Error(`Prop 'id' is required`);
 
-    useEffect(() => {
-        setError(false);
-    }, [value]);
+    useEffect(() => {}, [val, forwardedRef, error]);
 
     return (
         <div className='flex flex-col-reverse gap-2 text-[16px] leading-[19px]'>
             <div className='flex flex-col gap-2'>
                 <textarea
                     id={id}
-                    onChange={(e) => setValue(e.target.value)}
+                    name={id}
+                    onChange={change}
+                    onBlur={blur}
+                    ref={forwardedRef}
                     placeholder={placeholder}
-                    value={value}
+                    value={val}
                     rows={30}
                     cols={30}
-                    type={type}
                     disabled={disabled}
-                    className={`peer ${value.length > 0 ? 'border-dark-grey-placeholder' : ''} ${
-                        err && !disabled ? 'border-error' : 'border-dark-grey-disabled'
+                    className={`peer ${val.length > 0 ? 'border-dark-grey-placeholder' : ''} ${
+                        error && !disabled ? 'border-error' : 'border-dark-grey-disabled'
                     } py-[10px] px-4 border-2 rounded-lg max-h-[135px]
                     placeholder:text-dark-grey-placeholder
                     hover:border-primary-hover focus:outline-none
                     focus:border-primary-base focus:shadow-hover focus:shadow-primary-base/30
                     disabled:border-dark-grey-disabled disabled:text-dark-grey-disabled`}
                 ></textarea>
-                {err && !disabled && <p className='text-xs text-error'>{error}</p>}
+                {error && !disabled && <p className='text-xs text-error'>{error}</p>}
             </div>
             <label
                 htmlFor={id}
-                className='text-sm font-medium peer-disabled:text-dark-grey-disabled pointer-events-none'
+                className='text-sm font-medium peer-disabled:text-dark-grey-disabled pointer-events-none capitalize'
             >
                 {label}
             </label>
