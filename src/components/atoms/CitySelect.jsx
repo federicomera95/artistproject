d4ca889a4220caae1298538905f1bf47db306588
Federico Merafina <federico.merafina@losectrl.it>
2023-03-10 12:57:53 +0100

    fix: modify change,val for city select

--- src/components/atoms/CitySelect.jsx
+++ src/components/atoms/CitySelect.jsx
@@ -1,23 +1,33 @@
+import { useEffect } from 'react';
 import { ArrowDown } from '../../assets/icons';
 
 const options = ['Seleziona dalla lista', 'Torino', 'Milano', 'Roma', 'Napoli', 'Palermo'];
 
-const selectId = 'city-select';
+const CitySelect = ({ selectProps }) => {
+    const { id = 'city-select', val, error = '', change } = selectProps;
 
-const CitySelect = () => {
+    useEffect(() => {
+        console.log(val, 'inside select');
+    }, [val, error]);
+
     return (
         <div className='flex flex-col gap-2'>
-            <label htmlFor={selectId} className='text-dark-grey-base text-sm font-medium'>
+            <label htmlFor={id} className='text-dark-grey-base text-sm font-medium'>
                 Città
             </label>
             <div className='flex relative items-center'>
                 <select
-                    id={selectId}
+                    id={id}
+                    name={id}
+                    onChange={change}
+                    value={val}
                     className='w-full py-[10px] pl-4 pr-3 z-10 border-2 rounded-lg text-dark-grey-placeholder
                   border-dark-grey-disabled bg-transparent appearance-none outline-none'
                 >
                     {options.map((opt) => (
-                        <option key={options.indexOf(opt)}>{opt}</option>
+                        <option key={options.indexOf(opt)} value={opt}>
+                            {opt}
+                        </option>
                     ))}
                 </select>
                 <div className='absolute right-[10px]'>
