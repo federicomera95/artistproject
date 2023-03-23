3e44c7c2dbc6d447da7083ff7e199e4bfa240a73
Federico Merafina <federico.merafina@losectrl.it>
2023-03-23 15:13:29 +0100

    fix: add switch for photo,video,audio extended card

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,19 +3,147 @@ import ExtendedAudioCard from '../components/molecules/ExtendedAudioCard';
 import ExtendedVideoCard from '../components/molecules/ExtendedVideoCard';
 
 const Explore = () => {
+    const contents = [
+        {
+            id: 1,
+            type: 'photo',
+            username: 'Giuseppe',
+            title: 'Anche oggi viva la legalità',
+            avatar: '',
+            image: '',
+            tags: ['Rock', 'Blues', 'Chitarra'],
+            description:
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.'
+        },
+        {
+            id: 2,
+            type: 'audio',
+            avatar: '',
+            username: 'Matteo',
+            title: 'Audio bellissimo',
+            description:
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+            thumbnail: '',
+            audio: '/media/town-10169.mp3',
+            tags: ['Rock', 'Blues', 'Chitarra']
+        },
+        {
+            id: 3,
+            type: 'audio',
+            avatar: '',
+            username: 'Matteo',
+            title: 'Audio bruttissimo',
+            thumbnail: '',
+            description:
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+            audio: '/media/town-10169.mp3',
+            tags: ['Rock', 'Blues', 'Chitarra']
+        },
+        {
+            id: 4,
+            type: 'photo',
+            username: 'Giuseppe',
+            title: 'Anche oggi viva la legalità',
+            avatar: '',
+            image: '',
+            tags: ['Rock', 'Blues', 'Chitarra'],
+            description:
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.'
+        },
+        {
+            id: 5,
+            type: 'video',
+            username: 'Matteo',
+            title: 'Video bellissimo',
+            tags: ['Rock', 'Blues', 'Chitarra'],
+            thumbnail: '',
+            description:
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+            avatar: '',
+            video: '/media/sample-video.mp4'
+        },
+        {
+            id: 6,
+            type: 'photo',
+            username: 'Giuseppe',
+            title: 'Anche oggi viva la legalità',
+            avatar: '',
+            image: '',
+            tags: ['Rock', 'Blues', 'Chitarra'],
+            description:
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.'
+        },
+        {
+            id: 7,
+            type: 'video',
+            username: 'Matteo',
+            title: 'Video bellissimo',
+            tags: ['Rock', 'Blues', 'Chitarra'],
+            thumbnail: '',
+            description:
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+            avatar: '',
+            video: '/media/sample-video-2.mp4'
+        },
+        {
+            id: 8,
+            type: 'video',
+            username: 'Matteo',
+            title: 'Video bellissimo',
+            tags: ['Rock', 'Blues', 'Chitarra'],
+            thumbnail: '',
+            description:
+                'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+            avatar: '',
+            video: '/media/sample-video-3.mp4'
+        }
+    ];
+
     return (
         <>
             <div className='flex flex-col items-center gap-y-10 pb-20'>
-                <ExtendedPhotoCard title='placeholder 1' username='User 1' />
-                <ExtendedPhotoCard title='placeholder 2' username='User 2' />
-                <ExtendedPhotoCard title='placeholder 3' username='User 3' />
-                <ExtendedAudioCard title='placeholder 4' username='user 4' />
-                <ExtendedAudioCard title='placeholder 5' username='user 5' />
-                <ExtendedAudioCard title='placeholder 6' username='user 6' />
-                <ExtendedVideoCard title='placeholder 7' name='user 7' />
-                <ExtendedVideoCard title='placeholder 8' name='user 8' />
-                <ExtendedVideoCard title='placeholder 9' name='user 9' />
-                <ExtendedVideoCard title='placeholder 10' name='user 10' />
+                {[...contents].map((content, i) => {
+                    switch (content.type) {
+                        case 'photo':
+                            return (
+                                <ExtendedPhotoCard
+                                    key={`${content.title}-${i}`}
+                                    avatar={content.avatar}
+                                    image={content.image}
+                                    title={content.title}
+                                    username={content.username}
+                                    description={content.description}
+                                    tags={content.tags}
+                                />
+                            );
+                        case 'audio':
+                            return (
+                                <ExtendedAudioCard
+                                    key={`${content.title}-${i}`}
+                                    username={content.username}
+                                    title={content.title}
+                                    thumbnail={content.thumbnail}
+                                    description={content.description}
+                                    audio={content.audio}
+                                    tags={content.tags}
+                                    avatar={content.avatar}
+                                />
+                            );
+                        case 'video':
+                            return (
+                                <ExtendedVideoCard
+                                    key={`${content.title}-${i}`}
+                                    username={content.username}
+                                    thumbnail={content.thumbnail}
+                                    title={content.title}
+                                    description={content.description}
+                                    avatar={content.avatar}
+                                    video={content.video}
+                                    tags={content.tags}
+                                />
+                            );
+                    }
+                })}
             </div>
         </>
     );
