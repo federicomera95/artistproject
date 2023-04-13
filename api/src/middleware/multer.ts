7421f0e4f5a86d6effcb994019f1b8645a4e2824
Matteo <ricciottimatteo2003@gmail.com>
2023-04-13 10:28:29 +0200

    remove mime from multer middleware

--- api/src/middleware/multer.ts
+++ api/src/middleware/multer.ts
@@ -1,71 +1,64 @@
-import multer, { Options } from "multer";
-import { randomUUID } from "crypto";
+import path from 'path';
+import multer from 'multer';
+import { randomUUID } from 'crypto';
 
-import mime from "mime";
-import { MimeType } from "../types";
-import { Request } from "express";
-
-export const generateFileName = (mimeType: string) => {
-  const randomFileName = `${randomUUID()}-${Date.now()}`;
-  const fileExtension = mime.getExtension(mimeType);
-  const fileName = `${randomFileName}.${fileExtension}`;
-
-  return fileName;
-};
+import { MimeType } from '../types';
+import { Request } from 'express';
+import { FileFilterCallback } from 'multer';
 
 const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, callback) => {
-    return callback(null, generateFileName(file.mimetype));
-  },
+	destination: (req, file, callback) => {
+		callback(null, path.join(__dirname, '../../files'));
+	},
+	filename: (req, file, callback) => {
+		const fileName = `${randomUUID()}-${Date.now()}${path.extname(file.originalname)}`;
+
+		callback(null, fileName);
+	}
 });
 
 const MAX_SIZE_IN_MEGABYTES: MimeType<number> = {
-  image: 6 * 1024 * 1024,
-  video: 0,
-  audio: 10 * 1024 * 1024,
+	image: 6 * 1024 * 1024,
+	video: 0,
+	audio: 10 * 1024 * 1024
 };
 
 const VALID_MIME_TYPES: MimeType<string[]> = {
-  image: ["image/png", "image/jpeg"],
-  video: ["video/mp4"],
-  audio: ["audio/mpeg", "audio/mp3"],
+	image: ['image/png', 'image/jpeg'],
+	video: ['video/mp4'],
+	audio: ['audio/mpeg', 'audio/mp3']
 };
 
 export const multerOptions = (
-  type: keyof MimeType<string[] | number>,
-  type2?: keyof MimeType<string[] | number>
+	type: keyof MimeType<string[] | number>,
+	type2?: keyof MimeType<string[] | number>
 ) => {
-  return {
-    fileFilter: (
-      _: any,
-      file: Express.Multer.File,
-      callback: multer.FileFilterCallback
-    ) => {
-      if (type2) {
-        if (
-          VALID_MIME_TYPES[type].includes(file.mimetype) ||
-          VALID_MIME_TYPES[type2].includes(file.mimetype)
-        ) {
-          return callback(null, true);
-        } else {
-          callback(null, false);
-        }
-      } else {
-        if (VALID_MIME_TYPES[type].includes(file.mimetype)) {
-          return callback(null, true);
-        } else {
-          callback(null, false);
-        }
-      }
-    },
-    limits: { fileSize: MAX_SIZE_IN_MEGABYTES[type] },
-  };
+	return {
+		fileFilter: (_: Request, file: Express.Multer.File, callback: FileFilterCallback) => {
+			if (type2) {
+				if (
+					VALID_MIME_TYPES[type].includes(file.mimetype) ||
+					VALID_MIME_TYPES[type2].includes(file.mimetype)
+				) {
+					return callback(null, true);
+				} else {
+					callback(null, false);
+				}
+			} else {
+				if (VALID_MIME_TYPES[type].includes(file.mimetype)) {
+					return callback(null, true);
+				} else {
+					callback(null, false);
+				}
+			}
+		},
+		limits: { fileSize: MAX_SIZE_IN_MEGABYTES[type] }
+	};
 };
 
-export const initMulterMiddleware = (
-  type: keyof MimeType<string[] | number>,
-  type2?: keyof MimeType<string[] | number>
+export const initMulter = (
+	type: keyof MimeType<string[] | number>,
+	type2?: keyof MimeType<string[] | number>
 ) => {
-  return multer({ storage, ...multerOptions(type, type2) });
+	return multer({ storage, ...multerOptions(type, type2) });
 };
--- api/src/routes/profile.ts
+++ api/src/routes/profile.ts
@@ -1,193 +1,189 @@
-import express, { Response, Router } from "express";
-import { RequestArtistAll } from "../types";
-import { User } from "../db";
-import { UserFields } from "../db/models/User";
-import Joi, { ValidationError } from "joi";
-import { email, handleErrors, phone } from "../utilities";
-import { initMulterMiddleware } from "../middleware/multer";
+import express, { Response, Router } from 'express';
+import { RequestArtistAll } from '../types';
+import { User } from '../db';
+import { UserFields } from '../db/models/User';
+import Joi, { ValidationError } from 'joi';
+import { email, handleErrors, phone } from '../utilities';
+import { initMulter } from '../middleware/multer';
 
-const uploadPhoto = initMulterMiddleware("image");
-const uploadMultiAudio = initMulterMiddleware("audio", "image");
+const uploadPhoto = initMulter('image');
+const uploadMultiAudio = initMulter('audio', 'image');
 
 const router = Router();
 
-router.get("/:user", async (req: RequestArtistAll, res: Response) => {
-  const { user } = req.params;
+router.get('/:user', async (req: RequestArtistAll, res: Response) => {
+	const { user } = req.params;
 
-  const artist = (await User.findOne(
-    { username: user, type: "artist" },
-    "-__v -_id -password -publicKey -email -type"
-  ).lean()) as UserFields;
+	const artist = (await User.findOne(
+		{ username: user, type: 'artist' },
+		'-__v -_id -password -publicKey -email -type'
+	).lean()) as UserFields;
 
-  if (!artist) return res.sendStatus(404);
+	if (!artist) return res.sendStatus(404);
 
-  res.status(203).json(artist);
+	res.status(203).json(artist);
 });
 
-router.put(
-  "/",
-  uploadPhoto.single("avatar"),
-  async (req: RequestArtistAll, res: Response) => {
-    try {
-      //get artist user from middleware
-      const artist = req.data!.artist as UserFields;
+router.put('/', uploadPhoto.single('avatar'), async (req: RequestArtistAll, res: Response) => {
+	try {
+		//get artist user from middleware
+		const artist = req.data!.artist as UserFields;
 
-      const JoiSchema = Joi.object().keys({
-        username: Joi.string().required(),
-        bio: Joi.string().optional().allow(""),
-        avatar: Joi.object().optional().allow(""),
-        city: Joi.string().required(),
-        age: Joi.number().required(),
-        gender: Joi.string().required(),
-      });
+		const JoiSchema = Joi.object().keys({
+			username: Joi.string().required(),
+			bio: Joi.string().optional().allow(''),
+			avatar: Joi.object().optional().allow(''),
+			city: Joi.string().required(),
+			age: Joi.number().required(),
+			gender: Joi.string().required()
+		});
 
-      // Validate request body
-      const data = await JoiSchema.validateAsync(req.body);
+		// Validate request body
+		const data = await JoiSchema.validateAsync(req.body);
 
-      artist.username = data.username;
-      artist.info.bio = data.bio;
-      artist.info.city = data.city;
-      artist.info.age = data.age;
-      artist.info.gender = data.gender;
-      if (req.file) {
-        artist.info.avatar = req.file.filename;
-      }
+		artist.username = data.username;
+		artist.info.bio = data.bio;
+		artist.info.city = data.city;
+		artist.info.age = data.age;
+		artist.info.gender = data.gender;
+		if (req.file) {
+			artist.info.avatar = req.file.filename;
+		}
 
-      await artist.save();
+		await artist.save();
 
-      res.status(200).json({ msg: "Profile modified" });
-    } catch (error) {
-      return res.sendStatus(404);
-    }
-  }
-);
+		res.status(200).json({ msg: 'Profile modified' });
+	} catch (error) {
+		return res.sendStatus(404);
+	}
+});
 
-router.put("/links", async (req: RequestArtistAll, res: Response) => {
-  try {
-    //get artist user from middleware
-    const artist = req.data!.artist as UserFields;
+router.put('/links', async (req: RequestArtistAll, res: Response) => {
+	try {
+		//get artist user from middleware
+		const artist = req.data!.artist as UserFields;
 
-    const JoiSchema = Joi.object()
-      .keys({
-        spotify: Joi.string().optional().allow(""),
-        instagram: Joi.string().optional().allow(""),
-        facebook: Joi.string().optional().allow(""),
-        tiktok: Joi.string().optional().allow(""),
-        youtube: Joi.string().optional().allow(""),
-        applemusic: Joi.string().optional().allow(""),
-        amazonmusic: Joi.string().optional().allow(""),
-        email: Joi.string().optional().allow("").pattern(email),
-        phone: Joi.string().optional().allow("").pattern(phone),
-      })
-      .length(9);
+		const JoiSchema = Joi.object()
+			.keys({
+				spotify: Joi.string().optional().allow(''),
+				instagram: Joi.string().optional().allow(''),
+				facebook: Joi.string().optional().allow(''),
+				tiktok: Joi.string().optional().allow(''),
+				youtube: Joi.string().optional().allow(''),
+				applemusic: Joi.string().optional().allow(''),
+				amazonmusic: Joi.string().optional().allow(''),
+				email: Joi.string().optional().allow('').pattern(email),
+				phone: Joi.string().optional().allow('').pattern(phone)
+			})
+			.length(9);
 
-    // Validate request body
-    const data = await JoiSchema.validateAsync(req.body);
+		// Validate request body
+		const data = await JoiSchema.validateAsync(req.body);
 
-    const { info } = artist;
+		const { info } = artist;
 
-    info.links = data;
+		info.links = data;
 
-    await artist.save();
+		await artist.save();
 
-    res.status(200).json({ msg: "Informations modified" });
-  } catch (error) {
-    handleErrors(error as ValidationError, res);
-  }
+		res.status(200).json({ msg: 'Informations modified' });
+	} catch (error) {
+		handleErrors(error as ValidationError, res);
+	}
 });
 
 router.post(
-  "/add-audio",
-  uploadMultiAudio.fields([
-    { name: "audio", maxCount: 1 },
-    { name: "thumbnail", maxCount: 1 },
-  ]),
-  async (req: RequestArtistAll, res: Response) => {
-    try {
-      //get artist user from middleware
-      const artist = req.data!.artist as UserFields;
+	'/add-audio',
+	uploadMultiAudio.fields([
+		{ name: 'audio', maxCount: 1 },
+		{ name: 'thumbnail', maxCount: 1 }
+	]),
+	async (req: RequestArtistAll, res: Response) => {
+		try {
+			//get artist user from middleware
+			const artist = req.data!.artist as UserFields;
 
-      const JoiSchema = Joi.object().keys({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        instruments: Joi.array().optional().allow(""),
-        genres: Joi.array().optional().allow(""),
-      });
+			const JoiSchema = Joi.object().keys({
+				title: Joi.string().required(),
+				description: Joi.string().required(),
+				instruments: Joi.array().optional().allow(''),
+				genres: Joi.array().optional().allow('')
+			});
 
-      // // Validate request body
-      const data = await JoiSchema.validateAsync(req.body);
+			// // Validate request body
+			const data = await JoiSchema.validateAsync(req.body);
 
-      data.type = "audio";
-      data.file = req.files;
+			data.type = 'audio';
+			data.file = req.files;
 
-      artist.contents.push(data);
-      await artist.save();
-      res.status(200).json({ msg: "Content audio created" });
-    } catch (error) {
-      res.status(404).json(error);
-    }
-  }
+			artist.contents.push(data);
+			await artist.save();
+			res.status(200).json({ msg: 'Content audio created' });
+		} catch (error) {
+			res.status(404).json(error);
+		}
+	}
 );
 router.post(
-  "/add-video",
-  uploadMultiAudio.fields([
-    { name: "video", maxCount: 1 },
-    { name: "thumbnail", maxCount: 1 },
-  ]),
-  async (req: RequestArtistAll, res: Response) => {
-    try {
-      //get artist user from middleware
-      const artist = req.data!.artist as UserFields;
+	'/add-video',
+	uploadMultiAudio.fields([
+		{ name: 'video', maxCount: 1 },
+		{ name: 'thumbnail', maxCount: 1 }
+	]),
+	async (req: RequestArtistAll, res: Response) => {
+		try {
+			//get artist user from middleware
+			const artist = req.data!.artist as UserFields;
 
-      const JoiSchema = Joi.object().keys({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        instruments: Joi.array().optional().allow(""),
-        genres: Joi.array().optional().allow(""),
-      });
+			const JoiSchema = Joi.object().keys({
+				title: Joi.string().required(),
+				description: Joi.string().required(),
+				instruments: Joi.array().optional().allow(''),
+				genres: Joi.array().optional().allow('')
+			});
 
-      // // Validate request body
-      const data = await JoiSchema.validateAsync(req.body);
+			// // Validate request body
+			const data = await JoiSchema.validateAsync(req.body);
 
-      data.type = "video";
-      data.file = req.files; //capture file video and thumbnail
+			data.type = 'video';
+			data.file = req.files; //capture file video and thumbnail
 
-      artist.contents.push(data);
-      await artist.save();
-      res.status(200).json({ msg: "Content audio created" });
-    } catch (error) {
-      res.status(404).json(error);
-    }
-  }
+			artist.contents.push(data);
+			await artist.save();
+			res.status(200).json({ msg: 'Content audio created' });
+		} catch (error) {
+			res.status(404).json(error);
+		}
+	}
 );
 router.post(
-  "/add-photo",
-  uploadPhoto.single("photo"),
-  async (req: RequestArtistAll, res: Response) => {
-    try {
-      //get artist user from middleware
-      const artist = req.data!.artist as UserFields;
+	'/add-photo',
+	uploadPhoto.single('photo'),
+	async (req: RequestArtistAll, res: Response) => {
+		try {
+			//get artist user from middleware
+			const artist = req.data!.artist as UserFields;
 
-      const JoiSchema = Joi.object().keys({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        instruments: Joi.array().optional().allow(""),
-        genres: Joi.array().optional().allow(""),
-      });
+			const JoiSchema = Joi.object().keys({
+				title: Joi.string().required(),
+				description: Joi.string().required(),
+				instruments: Joi.array().optional().allow(''),
+				genres: Joi.array().optional().allow('')
+			});
 
-      // // Validate request body
-      const data = await JoiSchema.validateAsync(req.body);
+			// // Validate request body
+			const data = await JoiSchema.validateAsync(req.body);
 
-      data.type = "photo";
-      data.file = req.file?.filename;
+			data.type = 'photo';
+			data.file = req.file?.filename;
 
-      artist.contents.push(data);
-      await artist.save();
-      res.status(200).json({ msg: "Content photo created" });
-    } catch (error) {
-      res.status(404).json(error);
-    }
-  }
+			artist.contents.push(data);
+			await artist.save();
+			res.status(200).json({ msg: 'Content photo created' });
+		} catch (error) {
+			res.status(404).json(error);
+		}
+	}
 );
 
 export default router;
