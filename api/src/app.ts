234a6dea45e71b59772a044b4ce33a80a62535d5
Federico Merafina <federico.merafina@losectrl.it>
2023-04-13 10:35:34 +0200

    feat: fixed express static name

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,13 +1,13 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 /* Base */
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
 
 /* API routes */
-import apiRoute from './routes';
-import { connect } from './db';
+import apiRoute from "./routes";
+import { connect } from "./db";
 
 const app: Express = express();
 const PORT = process.env.PORT!;
@@ -18,13 +18,15 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Server running');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server running");
 });
 
-app.use('/api', apiRoute);
-app.use('/avatar', express.static('uploads'));
+app.use("/api", apiRoute);
+app.use("/files", express.static("files"));
 
 connect();
 
-app.listen(PORT, () => console.log(`⚡️[server]: Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`⚡️[server]: Server running on http://localhost:${PORT}`)
+);
