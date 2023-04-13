77fb7ca1388f4b818d16d0cbaeba32b53fcde881
Federico Merafina <federico.merafina@losectrl.it>
2023-04-13 11:06:58 +0200

    feat: add max size video

--- api/src/middleware/multer.ts
+++ api/src/middleware/multer.ts
@@ -1,64 +1,70 @@
-import path from 'path';
-import multer from 'multer';
-import { randomUUID } from 'crypto';
+import path from "path";
+import multer from "multer";
+import { randomUUID } from "crypto";
 
-import { MimeType } from '../types';
-import { Request } from 'express';
-import { FileFilterCallback } from 'multer';
+import { MimeType } from "../types";
+import { Request } from "express";
+import { FileFilterCallback } from "multer";
 
 const storage = multer.diskStorage({
-	destination: (req, file, callback) => {
-		callback(null, path.join(__dirname, '../../files'));
-	},
-	filename: (req, file, callback) => {
-		const fileName = `${randomUUID()}-${Date.now()}${path.extname(file.originalname)}`;
+  destination: (req, file, callback) => {
+    callback(null, path.join(__dirname, "../../files"));
+  },
+  filename: (req, file, callback) => {
+    const fileName = `${randomUUID()}-${Date.now()}${path.extname(
+      file.originalname
+    )}`;
 
-		callback(null, fileName);
-	}
+    callback(null, fileName);
+  },
 });
 
 const MAX_SIZE_IN_MEGABYTES: MimeType<number> = {
-	image: 6 * 1024 * 1024,
-	video: 0,
-	audio: 10 * 1024 * 1024
+  image: 6 * 1024 * 1024,
+  video: 20 * 1024 * 1024,
+  audio: 10 * 1024 * 1024,
 };
 
 const VALID_MIME_TYPES: MimeType<string[]> = {
-	image: ['image/png', 'image/jpeg'],
-	video: ['video/mp4'],
-	audio: ['audio/mpeg', 'audio/mp3']
+  image: ["image/png", "image/jpeg"],
+  video: ["video/mp4"],
+  audio: ["audio/mpeg", "audio/mp3"],
 };
 
 export const multerOptions = (
-	type: keyof MimeType<string[] | number>,
-	type2?: keyof MimeType<string[] | number>
+  type: keyof MimeType<string[] | number>,
+  type2?: keyof MimeType<string[] | number>
 ) => {
-	return {
-		fileFilter: (_: Request, file: Express.Multer.File, callback: FileFilterCallback) => {
-			if (type2) {
-				if (
-					VALID_MIME_TYPES[type].includes(file.mimetype) ||
-					VALID_MIME_TYPES[type2].includes(file.mimetype)
-				) {
-					return callback(null, true);
-				} else {
-					callback(null, false);
-				}
-			} else {
-				if (VALID_MIME_TYPES[type].includes(file.mimetype)) {
-					return callback(null, true);
-				} else {
-					callback(null, false);
-				}
-			}
-		},
-		limits: { fileSize: MAX_SIZE_IN_MEGABYTES[type] }
-	};
+  return {
+    fileFilter: (
+      _: Request,
+      file: Express.Multer.File,
+      callback: FileFilterCallback
+    ) => {
+      if (type2) {
+        if (
+          VALID_MIME_TYPES[type].includes(file.mimetype) ||
+          VALID_MIME_TYPES[type2].includes(file.mimetype)
+        ) {
+          return callback(null, true);
+        } else {
+          callback(null, false);
+        }
+      } else {
+        if (VALID_MIME_TYPES[type].includes(file.mimetype)) {
+          return callback(null, true);
+        } else {
+          callback(null, false);
+        }
+      }
+    },
+    limits: { fileSize: MAX_SIZE_IN_MEGABYTES[type] },
+  };
 };
 
 export const initMulter = (
-	type: keyof MimeType<string[] | number>,
-	type2?: keyof MimeType<string[] | number>
+  type: keyof MimeType<string[] | number>,
+  type2?: keyof MimeType<string[] | number>
 ) => {
-	return multer({ storage, ...multerOptions(type, type2) });
+  return multer({ storage, ...multerOptions(type, type2) });
 };
